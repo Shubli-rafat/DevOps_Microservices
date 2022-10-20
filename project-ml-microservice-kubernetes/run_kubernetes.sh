29e7ab4c28a902d3f9af07dc3524d41bc4d09bdb
Shubli-rafat <113903675+Shubli-rafat@users.noreply.github.com>
2022-10-21 03:40:22 +0530

    Update run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="shublirafat/udacityshub"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityshub \
+        --image="shublirafat/udacityshub" \
+        --port=80 --labels app=udacityshub
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityshub 8000:80
